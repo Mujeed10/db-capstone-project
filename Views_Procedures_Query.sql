5077461ea245f991eb04e803c84a6e121b3fa940
Mujeed Yakubu <mjd4real@gmail.com>
2023-12-02 22:37:33 -0600

    Adding update to view_prcedure query

--- Views_Procedures_Query.sql
+++ Views_Procedures_Query.sql
@@ -53,3 +53,15 @@ END;
 DELIMITER ;
 
 
+DELIMITER $$
+
+CREATE PROCEDURE UpdateBooking(IN booking_id INT, IN booking_date DATE)
+BEGIN
+    UPDATE bookings SET BookingDate = booking_date WHERE BookingID = booking_id; 
+    SELECT CONCAT('Booking ', booking_id, ' updated') AS Confirmation;
+END$$
+
+DELIMITER ;
+
+
+
